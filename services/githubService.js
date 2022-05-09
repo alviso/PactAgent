fd377c232bf3ab878981a7aeb39912f42c5d855b
Peter Varga <wmobil@gmail.com>
2022-05-08 18:38:38 -0700

    homepage improvements

--- services/githubService.js
+++ services/githubService.js
@@ -8,8 +8,13 @@ class githubService {
         const time = response.data[0].created_at
         const ago = moment(time).fromNow()
         const lastCommit = {number: ago.split(' ')[0], rest: ago.split(' ')[1] + ' ' + ago.split(' ')[2]}
-        if ((lastCommit.number.toLowerCase() === 'few')) lastCommit.number = 0
+        if (!this.isNumeric(lastCommit.number)) lastCommit.number = '1'
         return lastCommit
+    }
+    static isNumeric(str) {
+        if (typeof str != "string") return false // we only process strings!
+        return !isNaN(str) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
+            !isNaN(parseFloat(str)) // ...and ensure strings of whitespace fail
     }
 }
 
