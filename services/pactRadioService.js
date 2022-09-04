bc3c26be9afd93f9409447c13bb218ed43dfec57
Peter Varga <wmobil@gmail.com>
2022-09-03 22:18:39 -0700

    remove remaining local

--- services/pactRadioService.js
+++ services/pactRadioService.js
@@ -15,7 +15,7 @@ class pactRadioService {
         this.wallet = ''
         this.transferPw = ''
         this.nodes = []
-        this.closeFee = 25000
+        this.closeFee = 50000
         this.gatewayGPSCache = {}
         let KPString = "{}"
         try {
