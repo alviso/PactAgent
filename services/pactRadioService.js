356277f8e0c6c0a48432758f48c29c033f9dadaf
Peter Varga <wmobil@gmail.com>
2022-09-17 17:57:54 -0700

    fix for china

--- services/pactRadioService.js
+++ services/pactRadioService.js
@@ -45,6 +45,8 @@ class pactRadioService {
             this.rate = 1
         else if (this.instance.includes('NE'))
             this.rate = 1
+        else if (this.instance.includes('CN'))
+            this.rate = 1
         else this.rate = 0.2
 
         this.asKeyManage()
