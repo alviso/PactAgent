f36412dffad7c73f29a50e608938c66c0710211a
Peter Varga <wmobil@gmail.com>
2022-09-18 15:17:32 -0700

    further restrict for our proprietary message

--- services/chirpstackService.js
+++ services/chirpstackService.js
@@ -47,6 +47,7 @@ class chirpstackService {
             if (!payload?.macPayload?.bytes) return  //Not our proprietary perhaps
             const buff = new Buffer(payload.macPayload.bytes, 'base64');
             const gatewayId = buff.toString('ascii');
+            console.log('gateway id middle part', gatewayId.substr(6,3))
             if (gatewayId.substr(6,3) !== 'fff') return //Not our proprietary perhaps
             const rec = {mic: payload?.mic, gatewayId, ts:Date.now()}
             obj.uplinkFrame.senderGateway = gatewayId
