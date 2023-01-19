cd11d118761a3c3fd8bfccb34a27a59df2b02d6a
Peter Varga <wmobil@gmail.com>
2023-01-19 15:05:01 -0800

    switch back decrypt

--- services/pactRadioService.js
+++ services/pactRadioService.js
@@ -165,12 +165,12 @@ class pactRadioService {
                     const sendNode = checkableNodes[i]
                     sendNode.gps = await this.cS.getGatewayGPS(sendNode.gatewayId)
                     let sent = this.decrypt(asKey[0].priv, sendNode.sent)
-                    sent = '111111'
+                    // sent = '111111'
                     const resp = await this.pactCall('L', 'free.radio02.get-gateway', sendNode.gatewayId)
                     const receives = JSON.parse(resp.replaceAll('} {','},{')) || []
                     for (let j in receives) {
                         receives[j].mic = this.decrypt(asKey[0].priv, receives[j].mic)
-                        receives[j].mic = sent
+                        // receives[j].mic = sent
                         receives[j].gatewayId = sendNode.gatewayId
                     }
                     //Analyze and reward here
