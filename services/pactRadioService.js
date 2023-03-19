d61325641c25fbe220b8e2e6ce9e487f1dd31841
Peter Varga <wmobil@gmail.com>
2023-03-18 17:41:52 -0700

    adjust for add-received message

--- services/pactRadioService.js
+++ services/pactRadioService.js
@@ -76,7 +76,8 @@ class pactRadioService {
                         }
                     }
                     if (txns[i].type === 'free.radio02.add-received') {
-                        if (resp[txn].result?.status.includes('failure')) { //error?.message.includes('exceeded')) {
+                        if (resp[txn].result?.status.includes('failure')
+                            || resp[txn].result?.data.includes('Maximum')) { //error?.message.includes('exceeded')) {
                             if (this.rate >= 0.2) {
                                 this.rate -= 0.1
                                 console.log('Adjusting rate to:',this.rate)
