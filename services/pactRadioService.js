3fe8d49131fffbeebf886ebcba3011fe8fc7ef90
Peter Varga <wmobil@gmail.com>
2023-05-29 17:45:56 -0700

    compensate for too few consensus nodes

--- services/pactRadioService.js
+++ services/pactRadioService.js
@@ -67,8 +67,7 @@ class pactRadioService {
 
                     //TODO: This section is just extra logic
                     console.log(resp[txn].type, txn)
-                    console.log(resp[txn].gas, resp[txn].result)
-                    console.log(elapsedSec)
+                    console.log(resp[txn].gas, resp[txn].result, elapsedSec)
                     // console.log(txns[i], resp[txn].result?.error?.message)
                     if (txns[i].type === 'free.radio02.close-send-receive' && resp[txn].result?.error?.message.includes('exceeded')) {
                         const split = resp[txn].result.error.message.split('exceeded:')
